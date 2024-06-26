cd796e6c3aa543d8d94010912b23e21f8fe74f4a
AlexisOl <oleoalexis@gmail.com>
2024-06-25 20:15:19 -0600

    (cliente) ingreso de imagen

---
+++ bookstore-frontend/src/app/models/libros.ts
@@ -0,0 +1,12 @@
+export interface libros {
+  titulo: String;
+  autor: String;
+  descripcion: String;
+  genero: String;
+  fecha_publicacion: Date;
+  disponibilidad: Boolean;
+  cantidad_stock: Number;
+  puntuacion_promedio: Number;
+  precio: Number;
+  imagen_url: String;
+}
