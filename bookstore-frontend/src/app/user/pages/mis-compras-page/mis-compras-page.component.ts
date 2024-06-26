4558399e923306c34725a9f6a39584d2767e7df5
AlexisOl <oleoalexis@gmail.com>
2024-06-25 19:07:42 -0600

    para los pedidos

--- bookstore-frontend/src/app/user/pages/mis-compras-page/mis-compras-page.component.ts
+++ bookstore-frontend/src/app/user/pages/mis-compras-page/mis-compras-page.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-mis-compras-page',
   templateUrl: './mis-compras-page.component.html',
-  styles: [
-  ]
+  styleUrls: ['./pedidos.css'],
 })
-export class MisComprasPageComponent {
+export class MisComprasPageComponent implements OnInit {
+  totalItems = 100;
+  pageSize = 5;
+  pagedItems: any[] = [];
+  allItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+  ngOnInit() {
+    this.updatePagedItems(0, this.pageSize);
+  }
+
+  onPageChange(event: PageEvent) {
+    this.pageSize = event.pageSize;
+    const startIndex = event.pageIndex * event.pageSize;
+    this.updatePagedItems(startIndex, this.pageSize);
+  }
+
+  updatePagedItems(startIndex: number, pageSize: number) {
+    this.pagedItems = this.allItems.slice(startIndex, startIndex + pageSize);
+  }
 }
