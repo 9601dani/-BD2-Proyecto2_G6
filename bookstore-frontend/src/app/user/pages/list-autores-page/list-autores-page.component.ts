f74d3b35fa8d8575048e5272fc3dac651cedde89
AlexisOl <oleoalexis@gmail.com>
2024-06-25 19:29:41 -0600

    para el listado de autores (cliente)

--- bookstore-frontend/src/app/user/pages/list-autores-page/list-autores-page.component.ts
+++ bookstore-frontend/src/app/user/pages/list-autores-page/list-autores-page.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list-autores-page',
   templateUrl: './list-autores-page.component.html',
-  styles: [
-  ]
+  styleUrls: ['./listaAutores.css'],
 })
-export class ListAutoresPageComponent {
+export class ListAutoresPageComponent implements OnInit {
+  listadoAutores: string[] = [
+    'hola',
+    'adios',
+    'simon',
+    'preuba',
+    'aaa',
+    'hola',
+    'adios',
+    'simon',
+    'preuba',
+    'aaa',
+    'hola',
+    'adios',
+    'simon',
+    'preuba',
+    'aaa',
+    'final',
+  ];
 
+  // que mire el objeto de paginator
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  // tenemos el observable
+  obs!: Observable<any>;
+  // generamos la info
+  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>(
+    this.listadoAutores
+  );
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    // solo que detecte cambos y genere el paginator
+    this.changeDetectorRef.detectChanges();
+    this.dataSource.paginator = this.paginator;
+    this.obs = this.dataSource.connect();
+  }
 }
--- bookstore-frontend/src/app/user/pages/mis-compras-page/mis-compras-page.component.ts
+++ bookstore-frontend/src/app/user/pages/mis-compras-page/mis-compras-page.component.ts
@@ -12,16 +12,19 @@ export class MisComprasPageComponent implements OnInit {
   pagedItems: any[] = [];
   allItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+  //inicio
   ngOnInit() {
     this.updatePagedItems(0, this.pageSize);
   }
 
+  //con cambios
   onPageChange(event: PageEvent) {
     this.pageSize = event.pageSize;
     const startIndex = event.pageIndex * event.pageSize;
     this.updatePagedItems(startIndex, this.pageSize);
   }
 
+  // actualizacion de elementosd
   updatePagedItems(startIndex: number, pageSize: number) {
     this.pagedItems = this.allItems.slice(startIndex, startIndex + pageSize);
   }
