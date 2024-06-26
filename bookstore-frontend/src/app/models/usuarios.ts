5350741391cdfa2494e489c6fe16e8e9fdd13166
AlexisOl <oleoalexis@gmail.com>
2024-06-25 20:02:21 -0600

    (cliente) para autores ya por autor:id

---
+++ bookstore-frontend/src/app/models/usuarios.ts
@@ -0,0 +1,12 @@
+export interface usuarios {
+  username: String;
+  password: String;
+  complete_name: String;
+  email: String;
+  phone: String;
+  address: String;
+  register_date: Date;
+  rol: String;
+  photo: String;
+  payment_method: String;
+}
--- bookstore-frontend/src/app/user/pages/list-autores-page/list-autores-page.component.ts
+++ bookstore-frontend/src/app/user/pages/list-autores-page/list-autores-page.component.ts
@@ -3,29 +3,34 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
 
+interface valores {
+  id: number;
+  valor: string;
+}
+
 @Component({
   selector: 'app-list-autores-page',
   templateUrl: './list-autores-page.component.html',
   styleUrls: ['./listaAutores.css'],
 })
 export class ListAutoresPageComponent implements OnInit {
-  listadoAutores: string[] = [
-    'hola',
-    'adios',
-    'simon',
-    'preuba',
-    'aaa',
-    'hola',
-    'adios',
-    'simon',
-    'preuba',
-    'aaa',
-    'hola',
-    'adios',
-    'simon',
-    'preuba',
-    'aaa',
-    'final',
+  listadoAutores: valores[] = [
+    { id: 1, valor: 'hola' },
+    { id: 2, valor: 'adios' },
+    { id: 3, valor: 'simon' },
+    { id: 4, valor: 'prueba' },
+    { id: 5, valor: 'aaa' },
+    { id: 6, valor: 'hola' },
+    { id: 7, valor: 'adios' },
+    { id: 8, valor: 'simon' },
+    { id: 9, valor: 'prueba' },
+    { id: 10, valor: 'aaa' },
+    { id: 11, valor: 'hola' },
+    { id: 12, valor: 'adios' },
+    { id: 13, valor: 'simon' },
+    { id: 14, valor: 'prueba' },
+    { id: 15, valor: 'aaa' },
+    { id: 16, valor: 'final' },
   ];
 
   // que mire el objeto de paginator
@@ -33,7 +38,7 @@ export class ListAutoresPageComponent implements OnInit {
   // tenemos el observable
   obs!: Observable<any>;
   // generamos la info
-  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>(
+  dataSource: MatTableDataSource<valores> = new MatTableDataSource<valores>(
     this.listadoAutores
   );
 
@@ -44,5 +49,6 @@ export class ListAutoresPageComponent implements OnInit {
     this.changeDetectorRef.detectChanges();
     this.dataSource.paginator = this.paginator;
     this.obs = this.dataSource.connect();
+    console.log(this.dataSource.filteredData);
   }
 }
