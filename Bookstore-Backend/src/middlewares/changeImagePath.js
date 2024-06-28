0ae31bfd0396836d713573b2c8954cd0d374279b
Randy Sum <randy26smc@gmail.com>
2024-06-27 23:27:28 -0600

    backend afuera

--- Bookstore-Backend/src/middlewares/changeImagePath.js
+++
@@ -1,8 +0,0 @@
-const { response } = require('express')
-
-const changeImagePath = (req, res = response, next) => {
-    console.log('Middleware changeImagePath');
-    console.log(response.json)
-}
-
-module.exports = { changeImagePath };
