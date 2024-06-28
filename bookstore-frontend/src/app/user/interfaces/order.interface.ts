6f90098239b60d22aba7f4924c7b385bb98cca6f
AlexisOl <oleoalexis@gmail.com>
2024-06-28 05:23:07 -0600

    ultimos cambios

--- bookstore-frontend/src/app/user/interfaces/order.interface.ts
+++ bookstore-frontend/src/app/user/interfaces/order.interface.ts
@@ -1,19 +1,18 @@
-
 export interface Book {
-    _id: string;
-    id_libro: string;
-    subtotal: number;
-    cantidad: number;
-  }
-  
-  export interface Order {
-    _id: string;
-    fecha_pedido: string;
-    estado: string;
-    precio_total: number;
-    id_usuario: string;
-    direccion_envio: string;
-    libros: Book[];
-    metodo_pago: string;
-  }
-  
+  _id: string;
+  id_libro: string;
+  subtotal: number;
+  cantidad: number;
+  nombre: string;
+}
+
+export interface Order {
+  _id: string;
+  fecha_pedido: string;
+  estado: string;
+  precio_total: number;
+  id_usuario: string;
+  direccion_envio: string;
+  libros: Book[];
+  metodo_pago: string;
+}
